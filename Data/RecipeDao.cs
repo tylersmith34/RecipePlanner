7ab4f81bf81e70508ff083c85f679734302f0ff9
Tyler Smith <tylersmith.34@gmail.com>
2015-02-12 22:52:12 -0600

    Getting service to return recipes from DB

---
+++ Data/RecipeDao.cs
@@ -0,0 +1,55 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+	public class RecipeDao
+	{
+		private static RecipeDao instance;
+		private SqlConnection connection;
+
+		public static RecipeDao getInstance()
+		{
+			if (instance == null)
+			{
+				instance = new RecipeDao();
+				instance.connection = DataSourceFactory.getInstance().getDefaultConnection();
+			}
+			return instance;
+		}
+
+		public IEnumerable<Recipe> FindAllRecipes()
+		{
+			List<Recipe> recipes = new List<Recipe>();
+			String sql = "SELECT id, name, description FROM Recipe";
+
+			if (connection.State == System.Data.ConnectionState.Closed)
+			{
+				connection.Open();
+			}
+
+			SqlCommand command = new SqlCommand(sql, connection);
+			var reader = command.ExecuteReader();
+
+			if (reader.HasRows)
+			{
+				while (reader.Read())
+				{
+					var recipe = new Recipe();
+					recipe.Description = reader.GetString(2);
+					recipe.Name = reader.GetString(1);
+					recipe.Id = reader.GetInt32(0);
+					recipes.Add(recipe);
+				}
+				reader.Close();
+			}
+
+			return recipes;
+		}
+	}
+}
